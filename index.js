a287e273aab3d7bc8c04484ea839560c4aa4fa08
Frankline Machani <machanifranklin@gmail.com>
2025-06-08 11:33:49 +0300

    scope test

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+// 1. Declare customerName in global scope
+var customerName = 'bob';
+
+// 2. Function to uppercase the customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Function to set best customer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// 4. Function to overwrite best customer
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+// 5. Function that tries to change a constant (should throw error)
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
